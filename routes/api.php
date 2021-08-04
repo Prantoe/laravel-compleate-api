07b3fb406316034c36c75d8c20d2bed5cb5f14a4
Prantoe <suwarnopranto@gmail.com>
2021-08-05 04:09:16 +0700

    multiple create

--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,22 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\FormController;
+use App\Http\Controllers\API\ScoreController;
 
 
 Route::middleware(['auth:sanctum'])->group(function () {
+
+      // crud student
     Route::post('/create',[FormController::class, 'create']);
     Route::get('/edit/{id}',[FormController::class, 'edit']);
     Route::post('/edit/{id}',[FormController::class, 'update']);
       Route::get('/delete/{id}',[FormController::class, 'delete']);
-    Route::get('/logout', [AuthController::class, 'logout']);
+    
+
+//     crud score
+Route::any('/create-score-student', [ScoreController::class, 'create']);
+
+Route::get('/logout', [AuthController::class, 'logout']);
 });
 
 
